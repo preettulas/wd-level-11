b590cae809979c00af306d34abcedd2c39aba2db
Preet Kumar <113340889+preettulas@users.noreply.github.com>
2023-01-07 15:15:42 +0530

    Create election.js

---
+++ models/election.js
@@ -0,0 +1,72 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class election extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+  
+    static getElections(adminId) {
+      return this.findAll({
+        where:{
+          adminId,
+        },
+        order:[["id","ASC"]]
+      })
+    }
+    static startElection(id){
+      return this.update({
+        start:true,
+      },
+      {
+        where:{
+          id : id
+        }
+      })
+    }
+    static endElection(id){
+      return this.election.update({
+        end:true,
+      },
+      {
+        where:{
+          id:id
+        }
+      })
+    }
+    static addElection({elecName,publicurl,adminId}){
+      return this.create({
+        elecName,
+        publicurl,
+        adminId,
+      })
+    }
+  
+  static associate(models) {
+    // define association here
+    election.belongsTo(models.Admin, {
+      foreignKey: "adminId",
+    });
+    election.hasMany(models.question,{
+      foreignKey: "elecId"
+    });
+    election.hasMany(models.voters,{
+      foreignKey: "elecId"
+    })
+  }
+}
+  election.init({
+    elecName: DataTypes.STRING,
+    start: DataTypes.BOOLEAN,
+    end: DataTypes.BOOLEAN,
+    publicurl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'election',
+  });
+  return election;
+};
