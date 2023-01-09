d510ba17407166d42d366c9ab98e6b7fb491ee07
Preet Kumar <113340889+preettulas@users.noreply.github.com>
2023-01-09 09:51:38 +0530

    Update election.js

--- models/election.js
+++ models/election.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class election extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
   
     static getElections(adminId) {
       return this.findAll({
